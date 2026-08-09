c32e26d6c255ad96025ac81cb63858c60f7d3367
josemi1189 <jose1189@gmail.com>
2026-08-09 21:07:43 +0200

    chore: config base

--- Modulo_5/casas-rurales-tanstack/vite.config.ts
+++ Modulo_5/casas-rurales-tanstack/vite.config.ts
@@ -1,28 +1,24 @@
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+// vite.config.ts
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/assets/css/variables" as *;',
-      },
-    },
-  },
+  plugins: [tanstackStart(), react()],
   resolve: {
     alias: {
+      "#": "/src",
       "@": "/src",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/css/_variables" as *;`,
+      },
+    },
+    modules: {
+      localsConvention: "camelCase",
     },
   },
 });
